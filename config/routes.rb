a1a45c6174a12a11856772317acc1da13167d1c6
Doris Morales <moralesdoris880@gmail.com>
2022-05-15 19:01:24 -0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
   resources :entrys
-  # Defines the root path route ("/")
-  # root "articles#index"
+  user_entries
+
+  get '/user_entries', to: 'entrys#user_entries'
+
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
