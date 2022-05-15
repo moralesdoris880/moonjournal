7124c444e0ee991fc3b500ffc74ad93b8fe22af8
Doris Morales <moralesdoris880@gmail.com>
2022-05-15 18:21:40 -0400

    entry controller wip

--- app/controllers/entrys_controller.rb
+++ app/controllers/entrys_controller.rb
@@ -2,4 +2,64 @@ class EntrysController < ApplicationController
     wrap_parameters format:[]
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
+    # def index
+    #     user = User.find_by(id:session[:user_id])
+    #     if user
+    #         render json: Rating.all,include: [:user,:drink], status: :created
+    #     else
+    #         render json: {errors: ['Not found']}, status: :unauthorized
+    #     end
+    # end
+
+    # def user_ratings
+    #     user = User.find_by(id:session[:user_id])
+    #     if user
+    #         render json: user.ratings,include: [:drink], status: :accepted
+    #     else
+    #         render json: {errors: ['Not found']}, status: :unauthorized
+    #     end
+    # end
+
+    # def create
+    #     current_user = User.find_by(id:session[:user_id])
+    #     if current_user
+    #         rating = current_user.ratings.create!(rating_params)
+    #         render json: rating
+    #     else
+    #         render json: {errors: ["Not authorized"]}, status: :unauthorized
+    #     end
+    # end
+
+    # def update
+    #     user =  User.find_by(id: session[:user_id])
+    #     rating = user.ratings.find_by(id: params[:id])
+    #     if rating
+    #         rating.update(rating_params)
+    #         render json: rating,include: [:drink], status: :accepted
+    #     else
+    #         render json: {error: 'Not Found'}, status: :not_found
+    #     end
+    # end
+
+    # def destroy
+    #     user =  User.find_by(id: session[:user_id])
+    #     rating = user.ratings.find_by(id: params[:id])
+    #     if rating
+    #         rating.destroy
+    #         render json: {}
+    #     else
+    #         render json: {error: 'Not Found'}, status: :not_found
+    #     end
+    # end
+
+    # private
+
+    # def entry_params
+    #     params.permit(:comment,:drink_rating,:drink_id) 
+    # end
+
+    # def render_unprocessable_entity(invalid)
+    #     render json: { errors: ["Incorrect"]}, status: :unprocessable_entity
+    # end
+
 end
